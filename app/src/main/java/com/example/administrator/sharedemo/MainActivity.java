829fb71c4e01ee9b13dd9b98b45ba0967d710afc
WIN7-20161121ZY\Administrator <gino1993>
2017-02-08 17:24:49 +0800

    增加第三方登录，未完善

--- app/src/main/java/com/example/administrator/sharedemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/sharedemo/MainActivity.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.HashMap;
+
 import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.sharesdk.renren.Renren;
+import cn.sharesdk.sina.weibo.SinaWeibo;
 import cn.sharesdk.tencent.qq.QQ;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -20,18 +24,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         ShareSDK.initSDK(this);
         setContentView(R.layout.activity_main);
-        Button viewById = (Button) findViewById(R.id.hello);
-        viewById.setOnClickListener(this);
+        Button share = (Button) findViewById(R.id.share);
+        Button login = (Button) findViewById(R.id.login);
+        share.setOnClickListener(this);
+        login.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.share:
+                showShare();/** 各平台的第三方分享功能*/
+                //比如分享到QQ，其他平台则只需要更换平台类名，例如Wechat.NAME则是微信
+                //Platform plat = ShareSDK.getPlatform(QQ.NAME);
+                //showShare(plat.getName());/**自定义分享的平台*/
+                //showshare();/**在九宫格后添加分享的自定义图标*/
+                break;
+            case R.id.login:
+                authorize();
+                break;
+        }
 
-            //比如分享到QQ，其他平台则只需要更换平台类名，例如Wechat.NAME则是微信
-        Platform plat = ShareSDK.getPlatform(QQ.NAME);
-//        showShare(plat.getName());
-        showshare();
+
+    }
+    private void showShare() {
+        final OnekeyShare oks = new OnekeyShare();
+        //指定分享的平台，如果为空，还是会调用九宫格的平台列表界面
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        // title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
+        oks.setTitle("习习");
+        // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
+        oks.setTitleUrl("http://www.baidu.com");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+//        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+//        oks.setSite("ShareSDK");
+        oks.setSilent(true);   //隐藏编辑页面
+
+//        oks.setSilent(false);// 显示编辑页面
+
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+//        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
+
     }
     private void showShare(String platform) {
         final OnekeyShare oks = new OnekeyShare();
@@ -82,4 +129,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         oks.setCustomerLogo(enableLogo, label, listener);
         oks.show(this);
     }
+
+
+    private void authorize(){
+        Platform weibo = ShareSDK.getPlatform(QQ.NAME);
+//回调信息，可以在这里获取基本的授权返回的信息，但是注意如果做提示和UI操作要传到主线程handler里去执行
+        weibo.setPlatformActionListener(new PlatformActionListener() {
+
+            @Override
+            public void onError(Platform arg0, int arg1, Throwable arg2) {
+                // TODO Auto-generated method stub
+                arg2.printStackTrace();
+            }
+
+            @Override
+            public void onComplete(Platform arg0, int arg1, HashMap<String, Object> arg2) {
+                // TODO Auto-generated method stub
+                //输出所有授权信息
+                arg0.getDb().exportData();
+            }
+
+            @Override
+            public void onCancel(Platform arg0, int arg1) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+        //authorize与showUser单独调用一个即可
+                weibo.authorize();//单独授权,OnComplete返回的hashmap是空的
+                weibo.showUser(null);//授权并获取用户信息
+        //移除授权
+        //weibo.removeAccount(true);
+    }
+
 }
