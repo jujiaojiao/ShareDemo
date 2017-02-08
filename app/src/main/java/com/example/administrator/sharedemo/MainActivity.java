c90a9e67ef2db001bc6286888d9963a7663855e3
WIN7-20161121ZY\Administrator <gino1993>
2017-02-08 09:21:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/sharedemo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.administrator.sharedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ShareSDK.initSDK(this);
+        setContentView(R.layout.activity_main);
+        Button viewById = (Button) findViewById(R.id.hello);
+        viewById.setOnClickListener(this);
+    }
+
+
+
+    private void showShare() {
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        // title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
+        oks.setTitle("习习");
+        // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
+        oks.setTitleUrl("http://www.baidu.com");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+//        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+//        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+//        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        showShare();
+    }
+}
---
+++ app/src/main/java/com/example/administrator/sharedemo/com/example/administrator/sharedemo/apshare/ShareEntryActivity.java
@@ -0,0 +1,7 @@
+package com.example.administrator.sharedemo.com.example.administrator.sharedemo.apshare;
+
+import cn.sharesdk.alipay.utils.AlipayHandlerActivity;
+
+public class ShareEntryActivity extends AlipayHandlerActivity{
+
+}
